df6abacd136cd5bb709509dd8e0d227a17e311d9
LuanaAbramoff <62798202+LuanaAbramoff@users.noreply.github.com>
2020-04-03 09:38:44 -0300

    Primeira Parte do Código

---
+++ comentando.py
@@ -0,0 +1,7 @@
+# Jogo Craps
+# Nomes: Ana Barros e Luana Abramoff
+# Data: 2/4/2020
+# Interpretação do jogo: O jogador começa sempre pela fase Come Out. Nessa fase, ele tem quatro opções de tipo de aposta (Pass Line Bet, Field, Any Craps e Twelve), sendo 
+# que cada uma desses tipo de aposta tem suas regras. Se o jogador escolhe a Pass Line Bet, exite um conjunto de resultado dos dados que o leva automaticamente para uma fase
+# Point especial, que está diretamente conectada com o resultado dos dados da fase anterior (Come Out). Se na Come Out o jogador escolher qualquer outro tipo de aposta (Field, Any Craps ou Twelve)
+# ou nao entrar na condiçao específica da Pass Line Bet, ele segue para a fase Point normal, onde tem tres tipos de aposta disponível (Field, Any Craps e Twelve)
